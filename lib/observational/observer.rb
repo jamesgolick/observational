0cd99e153c7e5aac5e6b3b02d016cf78b9d21da8
James Golick <james@giraffesoft.ca>
2009-08-03 23:31:11 -0400

    make it possible to specify the parameters that are to be passed to the observing method

--- lib/observational/observer.rb
+++ lib/observational/observer.rb
@@ -1,15 +1,21 @@
 module Observational
   class Observer
-    attr_reader :subscriber, :method
+    attr_reader :subscriber, :method, :parameters
 
     def initialize(options)
       @subscriber = options[:subscriber]
       @method     = options[:method]
+      @parameters = options[:parameters]
     end
 
     def invoke(observable)
-      @subscriber.send(method, observable)
+      @subscriber.send(method, *arguments(observable))
     end
+
+    protected
+      def arguments(observable)
+        parameters.nil? ? observable : parameters.map { |p| observable.send(p) }
+      end
   end
 end
 
--- spec/observer_spec.rb
+++ spec/observer_spec.rb
@@ -3,13 +3,31 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Observer" do
   before do
     @klass    = Class.new
-    @observer = Observational::Observer.new :subscriber => @klass, 
-                                            :method     => :do_stuff
   end
 
-  it "should invoke the :method with the object" do
-    @klass.expects(:do_stuff).with(:the_object)
-    @observer.invoke(:the_object)
+  describe "without paraemeters specified" do
+    before do
+      @observer = Observational::Observer.new :subscriber => @klass, 
+                                              :method     => :do_stuff
+    end
+
+    it "should invoke the :method with the object" do
+      @klass.expects(:do_stuff).with(:the_object)
+      @observer.invoke(:the_object)
+    end
+  end
+
+  describe "with particular parameters" do
+    before do
+      @observer = Observational::Observer.new :subscriber => @klass,
+                                              :method     => :do_stuff,
+                                              :parameters => [:foo, :bar]
+    end
+    
+    it "should invoke the method with the parameters as specified" do
+      @klass.expects(:do_stuff).with("bar", "foo")
+      @observer.invoke(stub(:foo => "bar", :bar => "foo"))
+    end
   end
 end
 
