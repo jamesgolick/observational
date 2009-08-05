0460b3dfb1ac6df09c9e707364165716bf388951
James Golick <james@giraffesoft.ca>
2009-08-05 13:59:05 -0400

    doc Class#observes

--- lib/observational.rb
+++ lib/observational.rb
@@ -2,6 +2,17 @@ require 'observational/observer'
 require 'observational/observable'
 
 module Observational
+  ##
+  # Declares an observer
+  #
+  # @param [Symbol] the name of the model to observe
+  # @param [Hash] opts observer options
+  # @option opts [Symbol] :invokes The method to invoke on the subscriber.
+  # @option opts [Symbol] :with The parameters to pass to the method that gets invoked.
+  # @option opts [Symbol] :on The action to observe. e.g. :after_create for an active_record object
+  # @option opts [Symbol] :after A shortcut for AR objects. :after => :create is the equivalent of :on => :after_create
+  # @option opts [Symbol] :before Same as :after
+  #
   def observes(model_name, opts = {})
     opts.assert_valid_keys :with, :invokes, :on, :before, :after
 
